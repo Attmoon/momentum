c5d1fb9c8b13426cb494c5ad4c43d3a7df6b58bd
Attmoon <99157555+Attmoon@users.noreply.github.com>
2023-01-12 22:48:37 +0900

    function

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const player = {
-    name : "nico",
-    points : 10,
-    fat : true,
+function sayHello() {
+    console.log("Hello my name is C");
 }
-console.log(player);
-player.lastName = "potato";
-player.points = player.points + 15;
-console.log(player);
+
+sayHello("nico");
+sayHello("dal");
+sayHello("lynn");
