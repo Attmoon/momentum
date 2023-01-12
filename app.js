3869700890ef99c8a702aebeea7512332064deca
Attmoon <99157555+Attmoon@users.noreply.github.com>
2023-01-12 23:01:29 +0900

    function & parameter

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function sayHello() {
-    console.log("Hello my name is C");
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
 }
 
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+function divide(a, b) {
+    console.log(a / b);
+}
+
+plus(8, 60);
+divide(98, 20);
+
+const player = {
+    name : "nico",
+    sayHello : function(otherPersonsName) {
+        console.log("hello " + otherPersonsName + " nice to meet you");
+    }
+}
+
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
