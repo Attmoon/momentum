1874d987439a8d52a67b9c016f5be9d74e5f1d34
Attmoon <99157555+Attmoon@users.noreply.github.com>
2023-01-15 21:32:57 +0900

    return calculator

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const calculator = {
-    add : function(a, b) {
-        alert(a + b);
+    plus : function (a, b) {
+        return a + b;
     },
-    minus : function(a, b) {
-        alert(a - b);
+    minus : function (a, b) {
+        return a - b;
     },
-    divide : function(a, b) {
-        alert(a / b);
+    times : function (a, b) {
+        return a * b;
     },
-    powerof : function(a, b) {
-        alert(a ** b);
+    divide : function (a, b) {
+        return a / b;
+    },
+    power : function (a, b) {
+        return a ** b;
     }
 };
-calculator.add(1, 2);
-calculator.minus(1, 2);
-calculator.divide(1, 2);
-calculator.powerof(2, 2);
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
