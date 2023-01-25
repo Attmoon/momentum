1fe8a7ad33d1e6c31e8d278c038248645fa7ab45
Attmoon <99157555+Attmoon@users.noreply.github.com>
2023-01-25 22:07:14 +0900

    localStorage & save username

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HiDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add("hidden");
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerHTML = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HiDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HiDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
