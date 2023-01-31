5ee0af4b561e10a276bb82f8201919ad1a2e24e7
Attmoon <99157555+Attmoon@users.noreply.github.com>
2023-01-31 22:06:54 +0900

    id setting todo

--- js/todo.js
+++ js/todo.js
@@ -17,8 +17,9 @@ function deleteToTo(event) {
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToTo);
@@ -31,8 +32,12 @@ function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now()
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -43,5 +48,5 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+    parsedToDos.forEach(paintToDo);
 }
